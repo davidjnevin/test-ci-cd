c7f528d60309e71e6f1ba72b337f7a46225dd1c7
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-01 00:02:40 +0100

    Add main.py and test_main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# simple script to return the sum of a list of numbers using python library sum()
+
+# import the sum() function from the python library
+from functools import reduce
+
+# define a function to return the sum of a list of numbers
+def sum_list(list):
+    return reduce((lambda x, y: x + y), list)
+
+# define a list of numbers
+list = [1, 2, 3, 4, 5]
+
+
+if __name__ == "__main__":
+    sum_list(list)
