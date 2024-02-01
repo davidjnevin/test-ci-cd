9e7fc4accad7f294ab99064075df1a7cb34f2da6
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-01 01:54:31 +0100

    fix: format

--- src/numsum/main.py
+++ src/numsum/main.py
@@ -1,11 +1,14 @@
-# simple script to return the sum of a list of numbers using python library sum()
+# script to return the sum of a list of numbers
+# using python library reduce()
 
 # import the sum() function from the python library
 from functools import reduce
 
+
 # define a function to return the sum of a list of numbers
 def sum_list(list):
     return reduce((lambda x, y: x + y), list)
+
 
 # define a list of numbers
 list = [1, 2, 3, 4, 5]
